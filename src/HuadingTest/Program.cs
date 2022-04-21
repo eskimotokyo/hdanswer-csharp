32f4cb1c267e7534249372f74aa4cf320103dc31
yechengwei <yechengwei@96900.com.cn>
2022-04-21 10:27:34 +0800

    bugfix

--- src/HuadingTest/Program.cs
+++ src/HuadingTest/Program.cs
@@ -1,17 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using HuadingTest;
+using System.Collections.ObjectModel;
 
 Console.WriteLine("游戏开始!");
 var game = new PickUpGame<Toothpick>();
 Console.WriteLine("初始化排列为" + game.CurrentItemGroupString());
-var players = new string[] { "A", "B" };
+// 只读玩家列表
+var players = (new string[] { "A", "B" }).ToList().AsReadOnly();
+var playersNum = players.Count;
 int round = 1;
 
 while(!game.CheckEnded())
 {
     Console.WriteLine($"****操作回合{round}开始****");
-    var player = players[(round - 1) % 2];
-    Console.WriteLine($"玩家{player}，请输入需要选取的行序号……");
+    var player = players[(round - 1) % playersNum];
+    Console.WriteLine($"玩家【{player}】，请输入需要选取的行序号……");
     var row = Console.ReadLine();
     Console.WriteLine("请输入需要拿走的物品数量……");
     var take = Console.ReadLine();
@@ -27,12 +30,12 @@ while(!game.CheckEnded())
         }
         else
         { // 有异常
-            Console.WriteLine($"!!!!!!!!!!!操作失败: {result.Message}，请玩家{player}重新拿取!!!!!!!!!!!");
+            Console.WriteLine($"!!!!!!!!!!!操作失败: {result.Message}，请玩家【{player}】重新拿取!!!!!!!!!!!");
         }
     }
     else
     {
-        Console.WriteLine($"!!!!!!!!!!!必须输入整数，请玩家{player}重新拿取!!!!!!!!!!!");
+        Console.WriteLine($"!!!!!!!!!!!必须输入整数，请玩家【{player}】重新拿取!!!!!!!!!!!");
     }
 }
 Console.WriteLine("你输了！！");
